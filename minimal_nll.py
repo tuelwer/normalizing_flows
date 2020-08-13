f3c90951f22a07a9ed96c80143370de555c00295
Tobias Uelwer <tobias.uewer@hhu.de>
2020-08-13 10:42:47 +0200

    Extract std

--- minimal_nll.py
+++ minimal_nll.py
@@ -6,9 +6,14 @@ filename = sys.argv[1]
 f = open(filename, 'r')
 print('Open file:', filename)
 nlls = []
+stds = []
 for line in f:
     nll = re.findall(r"logp\(x\) = (.*) \+", line)
+    std = re.findall(r"\+\/\- (.*)", line)
+    print(std)
     if len(nll) > 0:
         nlls.append(float(nll[0]))
+        stds.append(float(std[0]))
 
-print('Minimum NLL:', min(nlls))
+print('Maximal Log-Likelihood:', max(nlls))
+print('Standard deviation:', stds[nlls.index(max(nlls))])
